2a8157a6ba5eba36bde6d86a3813b2db285dff44
theHinneh <theHinneh@theHinnehs-MacBook-Pro.local>
2021-10-23 22:58:00 +0000

    home

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  loggedIn: boolean = false
+  profileMenu: boolean = false
+  mobileMenu: boolean = false
+  linkItems: Array<string> = ['Services', 'Categories', 'Contact']
+  mobileMenuLinks: Array<string> = ['Your Profile', 'Settings', 'Sign out']
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  openProfileMenu(): void {
+    this.profileMenu = !this.profileMenu
+  }
+
+  openMobileMenu(): void {
+    this.mobileMenu = !this.mobileMenu
+  }
 }
