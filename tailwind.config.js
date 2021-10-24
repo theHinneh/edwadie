464595b99098bb051249c39286f641e259d87208
theHinneh <theHinneh@theHinnehs-MacBook-Pro.local>
2021-10-24 13:06:59 +0000

    tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +11,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer")
+  ],
 }
